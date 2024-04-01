4e8da003431f5f8d55aa114c88937d6938113271
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-01 19:16:21 +0700

    getline(cin, mhs.nim);

--- part 3/part 3.cpp
+++ part 3/part 3.cpp
@@ -14,3 +14,17 @@ struct Mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa :";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+	cout << "Alamat Mahasiswa :" << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.Desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.Kota;
+	cout << "Umur Mahasiswa :";
+	cin >> mhs.umur;
