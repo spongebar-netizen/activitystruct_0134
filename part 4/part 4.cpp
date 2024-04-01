ef5a6f1d3f31042950f1c50c7088e50fdfbe5704
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-01 19:22:27 +0700

    menggabungkan detail alamt dan data diri mahasiswa

---
+++ part 4/part 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+struct DetailAlamat {
+	string Desa;
+	string Kota;
+};
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
