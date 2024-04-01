d0abdee0aefb03fbcb85fb25b6ef010d696a16d2
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-01 19:24:25 +0700

    cin.ignore(1, '\n');

--- part 4/part 4.cpp
+++ part 4/part 4.cpp
@@ -16,3 +16,18 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
+		cout << "Data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor Mahasiswa :";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs[i].nama);
+		cout << "Alamat Mahasiswa :" << endl;
+		cout << "\t Nama Desa : ";
+		cin >> mhs[i].alamat.Desa;
+		cout << "\t Nama Kota : ";
+		cin >> mhs[i].alamat.Kota;
+		cout << "Umur Mahasiswa :";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
