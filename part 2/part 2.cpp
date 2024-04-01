301dd8eadc61a90ea9ced6adb2f11c1730c7533d
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-01 19:04:14 +0700

    menambahkan detail alamat

--- part 2/part 2.cpp
+++ part 2/part 2.cpp
@@ -15,3 +15,26 @@ struct Mahasiswa {
 	int umur;
 
 };
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa :";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa :" << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.Desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.Kota;
+	cout << "Umur Mahasiswa :";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\t \n Desa : " << mhs.alamat.Desa;
+	cout << "\n Kota : " << mhs.alamat.Kota;
+	cout << "\n Umur : " << mhs.umur;
+
+}
